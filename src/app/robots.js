32dcdf8ce44bc6bc9ad25d9cacfbde8973427ded
Sahil Bagul <joyboy7641@gmail.com>
2026-05-20 00:38:54 +0530

    fix: add all AI crawler rules to robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,11 +3,18 @@ const domain = process.env.NEXT_PUBLIC_SITE_URL || "https://smilecare.in";
 export default function robots() {
   return {
     rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/api/", "/_next/", "/static/"],
-      },
+      { userAgent: "*", allow: "/", disallow: ["/api/", "/_next/", "/static/"] },
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "Google-Extended", allow: "/" },
+      { userAgent: "Googlebot", allow: "/" },
+      { userAgent: "bingbot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "Amazonbot", allow: "/" },
+      { userAgent: "CCBot", allow: "/" },
+      { userAgent: "FacebookExternalHit", allow: "/" },
+      { userAgent: "Bytespider", allow: "/" },
+      { userAgent: "Applebot-Extended", allow: "/" },
     ],
     sitemap: `${domain}/sitemap.xml`,
     host: domain,
